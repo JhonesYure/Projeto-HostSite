0cae866cc8e2977423d3f76372d91e2f3f8f7561
Jhones Yure <jymbomfim@hotmail.com>
2022-06-25 14:05:36 -0300

    atualização

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip ()
+});
+
+//MODAL
+$('#meuModal').on('shown.bs.modal', function ()
+{
+    $('#meuInput').trigger('focus')
+})
